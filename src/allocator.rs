15ac7ca748d312522900786c9ac057ed2844f4e8
Anant Satya Mohit Kavuru <anant.kavuru@gmail.com>
2026-01-17 23:58:56 +0530

    test

--- src/allocator.rs
+++ src/allocator.rs
@@ -41,14 +41,14 @@ pub fn init_heap(
     Ok(())
 }
 
-pub struct Dummy;
+//pub struct Dummy;
 
-unsafe impl GlobalAlloc for Dummy {
-    unsafe fn alloc(&self, _layout: Layout) -> *mut u8 {
-        null_mut()
-    }
-
-    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
-        panic!("dealloc should be never called")
-    }
-}
+//unsafe impl GlobalAlloc for Dummy {
+//    unsafe fn alloc(&self, _layout: Layout) -> *mut u8 {
+//        null_mut()
+//    }
+//
+//    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
+//        panic!("dealloc should be never called")
+//    }
+//}
